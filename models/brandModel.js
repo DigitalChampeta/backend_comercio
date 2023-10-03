ff58ea6fc407cdffca538534dfb17d76f0523c77
digital.champeta@gmail.com <digital.champeta@gmail.com>
2023-10-03 15:41:41 +0200

    update changes for clear

--- models/brandModel.js
+++ models/brandModel.js
@@ -8,30 +8,6 @@ const BrandsModel = db.define("brands", {
 })
 export default BrandsModel;
 
-/*import { Product } from "./Product.js";
-import database from "../db.js";
-import { DataTypes } from "sequelize";
-
-export const Brand = database.define("brands", {
-    id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-    },
-    name: {
-    type: DataTypes.STRING,
-    }
-},
-{
-    timestamps: false,
-}
-);
-
-Brand.hasMany(Product, {
-foreignkey: "brandId",
-sourceKey: "id",
-});
-Product.belongsTo(Brand, { foreignkey: "brandId", targetId: "id" });*/
 
 
 
